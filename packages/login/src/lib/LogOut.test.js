8aa2e0f579dd0c9886d5f2d78e4dbb66d048c220
Michael Jewell <michaeljewell9911@gmail.com>
2019-04-20 13:44:29 -0700

    mj - add circleci

--- packages/login/src/lib/LogOut.test.js
+++ packages/login/src/lib/LogOut.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import LogOut from './LogOut';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<LogOut />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
